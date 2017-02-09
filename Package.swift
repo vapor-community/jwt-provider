416b8c0995e1091cc40d36601a65a08e2c2c4e43
Tanner Nelson <me@tanner.xyz>
2017-02-09 14:33:09 +0100

    specify minor

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporJWT",
     dependencies: [
-    	.Package(url:"https://github.com/vapor/jwt.git", majorVersion: 0),
+    .Package(url:"https://github.com/vapor/jwt.git", majorVersion: 0, minor: 7),
     	.Package(url:"https://github.com/vapor/vapor.git", majorVersion: 1),
     ]
 )
