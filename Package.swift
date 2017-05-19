ea8ab1736c4ec9f3db6b70247cbcf82183564c63
Siemen Sikkema <siemen@unclutter.nl>
2017-05-19 10:40:16 +0200

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@ let package = Package(
     name: "JWTProvider",
     dependencies: [
         // JSON Web Tokens in Swift by @siemensikkema.
-    	.Package(url:"https://github.com/vapor/jwt.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
+        .Package(url:"https://github.com/vapor/jwt.git", majorVersion: 2),
 
     	// Middleware and conveniences for using Auth in Vapor.
-    	.Package(url:"https://github.com/vapor/auth-provider.git", Version(1,0,0, prereleaseIdentifiers: ["beta"])),
+        .Package(url:"https://github.com/vapor/auth-provider.git", majorVersion: 1),
 
     	// A web framework and server for Swift that works on macOS and Ubuntu.
-    	.Package(url: "https://github.com/vapor/vapor.git", Version(2,0,0, prereleaseIdentifiers: ["beta"]))
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2)
     ]
 )
